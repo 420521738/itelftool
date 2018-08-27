5234898612fb03ae2081ddba64d4459b93874f4d
420521738 <420521738@qq.com>
2018-08-27 11:44:38 +0800

    任务编排中的ansible命令执行以及ansible的playbook执行功能完成并且添加注释

--- setup/ansible.py
+++ setup/ansible.py
@@ -21,7 +21,7 @@ level = get_dir("log_level")
 log_path = get_dir("log_path")
 log("setup.log", level, log_path)
 
-
+# 这是在判断变量不为空的时候，需要根据获取到的变量在对应的roles下创建vars/main.yml文件，并把变量写进去文件中
 def write_role_vars(roles, vargs):
 
     r_vars = vargs.split('\r\n')
@@ -33,7 +33,7 @@ def write_role_vars(roles, vargs):
             else:
                 os.mkdir(roles_dir+r+"/vars")
 
-            with open(roles_dir+r+'/vars/main.yml', 'wb+') as role_file:
+            with open(roles_dir+r+'/vars/main.yml', 'w+') as role_file:
                 role_file.writelines("---\n")
                 for x in r_vars:
                     rs = x + '\n'
@@ -53,8 +53,9 @@ def index(request):
 @login_required()
 def playbook(request):
     ret = []
-    if os.path.exists(ansible_dir + '/gexec.yml'):
-        os.remove(ansible_dir + '/gexec.yml')
+    # temp.yml是在页面选择roles时，就创建一个临时的playbook，仅做一次性执行，每次都会不一样
+    if os.path.exists(ansible_dir + '/temp.yml'):
+        os.remove(ansible_dir + '/temp.yml')
     else:
         pass
     if request.method == 'POST':
@@ -63,23 +64,28 @@ def playbook(request):
         pbook = request.POST.getlist('splaybook', [])
         roles = request.POST.getlist('mroles', [])
         role_vars = request.POST.get('mvars')
-
+        
+        # This 如果用户在页面选择的是主机
         if host:
+            # This 如果用户选择了角色，并为选择具体playbook
             if roles:
+                # This 如果用户选择了角色，并且设定了变量
                 if role_vars:
                     write_role_vars(roles, role_vars)
                 for h in host:
                     logging.info("==========ansible tasks start==========")
                     logging.info("User:"+request.user.name)
                     logging.info("host:"+h)
-                    with open(ansible_dir + '/gexec.yml', 'w+') as f:
-                        flist = ['- hosts: '+h+'\n', '  remote_user: root\n', '  gather_facts: true\n', '  roles:\n']
+                    # This 在/etc/ansible/下创建一个临时playbook，叫做temp.yml，并往这个临时文件中写入hosts，以及角色
+                    with open(ansible_dir + '/temp.yml', 'w+') as f:
+                        flist = ['- hosts: '+h+'\n', '  gather_facts: true\n', '  roles:\n']
                         for r in roles:
                             rs = '    - ' + r + '\n'
                             flist.append(rs)
                             logging.info("Role:"+r)
                         f.writelines(flist)
-                    cmd = "ansible-playbook"+" " + ansible_dir+'/gexec.yml'
+                    # This 执行刚刚创建的临时playbook，并将输出信息记录到date中，并返回到ret里面
+                    cmd = "ansible-playbook"+" " + ansible_dir+'/temp.yml'
                     p = Popen(cmd, stderr=PIPE, stdout=PIPE, shell=True)
                     data = p.communicate()
                     ret.append(data)
@@ -118,15 +124,15 @@ def playbook(request):
                     logging.info("==========ansible tasks start==========")
                     logging.info("User:"+request.user.name)
                     logging.info("group:"+g)
-                    f = open(ansible_dir + '/gexec.yml', 'w+')
-                    flist = ['- hosts: '+g+'\n', '  remote_user: root\n', '  gather_facts: true\n', '  roles:\n']
+                    f = open(ansible_dir + '/temp.yml', 'w+')
+                    flist = ['- hosts: '+g+'\n', '  gather_facts: true\n', '  roles:\n']
                     for r in roles:
                         rs = '    - ' + r + '\n'
                         flist.append(rs)
                         logging.info("Role:"+r)
                     f.writelines(flist)
                     f.close()
-                    cmd = "ansible-playbook"+" " + ansible_dir+'/gexec.yml'
+                    cmd = "ansible-playbook"+" " + ansible_dir+'/temp.yml'
                     p = Popen(cmd, stderr=PIPE, stdout=PIPE, shell=True)
                     data = p.communicate()
                     ret.append(data)
@@ -158,6 +164,7 @@ def playbook(request):
             return render(request, 'setup/result.html', locals())
 
 
+# This 如果用户在页面上选择执行ansible命令的话
 @login_required()
 def ansible_command(request):
     command_list = []
@@ -167,6 +174,7 @@ def ansible_command(request):
         mcommand = request.POST.get('mcommand')
         command_list = mcommand.split('\n')
         for command in command_list:
+            # This 命令可以是多个，但是不管多少个，命令必须都是以ansible开头的才算是合法的命令
             if command.startswith("ansible"):
                 p = Popen(command, stdout=PIPE, stderr=PIPE,shell=True)
                 data = p.communicate()
@@ -184,12 +192,14 @@ def ansible_command(request):
         return render(request, 'setup/result.html', locals())
 
 
+# This 这个功能是将组以及主机都查询出来，然后按照一定的格式，将查询出来的组以及主机写入ansible的hosts文件中
 @login_required()
 def host_sync(request):
     group = HostGroup.objects.all()
     ansible_file = open(ansible_dir+"/hosts", "w")
     all_host = Asset.objects.all()
     for host in all_host:
+        # This 因为资产表是有多个表通过外键关联的，ip信息在网卡表中，但是并非所有的网卡都会有IP地址，所以这里就先粗略获取到第一个IP地址
         nicinfo = NIC.objects.filter(asset_id = host.id)
         for i in nicinfo:
             if i.ipaddress is None:
